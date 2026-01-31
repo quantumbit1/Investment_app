0b44d031f9a8ae3d92fca312afc7e8795204ad7e
Kshitiz <kshitizyaksh@gmail.com>
2026-01-31 18:25:23 +0530

    added service files nav_fetcher done and xirr still need to implement

--- services/nav_fetcher.py
+++ services/nav_fetcher.py
@@ -1,1 +1,20 @@
 # Used to fetch the Mutual Fund Data from AMFI 
+
+import requests
+
+AMFI_URL = "https://portal.amfiindia.com/spages/NAVOpen.txt"
+
+class NAVFetcher:
+    #@staticmethod is used to define a method that doesn't need access to the instance (self) or class (cls)
+    @staticmethod
+    def get_latest_nav(scheme_code):
+        response = requests.get(AMFI_URL)
+        for line in reponse.text.splitlines():
+            if line.startswith(scheme_code + ";"):
+                parts = line.split(";")
+                nav = float(parts[4])
+                date = parts[5]
+                return nav , date
+        
+        return None, None
+            
--- services/xirr.py
+++ services/xirr.py
@@ -1,1 +1,10 @@
 # TODO : Financial Math
+
+class XIRRCalculator:
+    def calculate(self,cash_flows):
+        '''
+        cashflows: List of (date, amount)
+        returns: XIRR %
+        '''
+
+        raise NotImplementedError("XIRR calculation coming soon")
